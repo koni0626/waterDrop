56ecb2365f0d947d071a65f2e800727123ec624b
konishi <konickcioc@gmail.com>
2018-09-17 23:25:44 +0900

    まだまだ

--- waterDrop/waterDropApp/admin.py
+++ waterDrop/waterDropApp/admin.py
@@ -5,10 +5,11 @@ from django.contrib import admin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, BuTable, KaTable, CalendarTable, TimeCardTable, WorkCodeTable
-from .models import WorkDetailCodeTable, WorkTable, PriceTable, KubunTable, MeisaiTable, User
+from .models import WorkDetailCodeTable, WorkTable, PriceTable, HolidayKubunTable, User
+from .models import OfficeTable, TransportExpense, HowToMove, ApproveStatus
 
 
-
+'''社員情報の定義'''
 class UserModelAdmin(UserAdmin):
     list_display = ('username', 'last_name','first_name', 'kaCode')
 
@@ -27,11 +28,41 @@ class KaModelAdmin(admin.ModelAdmin):
 
 admin.site.register(KaTable, KaModelAdmin)
 
+'''タイムカードの定義'''
 admin.site.register(TimeCardTable)
-admin.site.register(CalendarTable)
+
+'''カレンダーの定義'''
+class CalendarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'Remark', "kubunid")
+
+admin.site.register(CalendarTable, CalendarModelAdmin)
+
+'''オフィスの定義'''
+admin.site.register(OfficeTable)
+
 admin.site.register(WorkCodeTable)
+
 admin.site.register(WorkDetailCodeTable)
+
 admin.site.register(WorkTable)
-admin.site.register(PriceTable)
-admin.site.register(KubunTable)
-admin.site.register(MeisaiTable)
+
+'''単価の定義'''
+class PriceModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tanka')
+
+admin.site.register(PriceTable, PriceModelAdmin)
+
+'''休暇区分の定義'''
+admin.site.register(HolidayKubunTable)
+
+'''交通手段の定義'''
+admin.site.register(HowToMove)
+
+'''審査承認ステータスの定義'''
+admin.site.register(ApproveStatus)
+
+'''交通費申請の定義'''
+class TransportModelAdmin(admin.ModelAdmin):
+    list_display = ('date', 'adddate', 'start', 'end', "expense")
+
+admin.site.register(TransportExpense, TransportModelAdmin)
--- waterDrop/waterDropApp/models.py
+++ waterDrop/waterDropApp/models.py
@@ -32,33 +32,7 @@ class KaTable(models.Model):
         return self.name
 
 '''
-class UserManager(BaseUserManager):
-    def create_user(self, employ_number, password=None, **extra_fields):
-        if not employ_number:
-            raise ValueError('Users must have a email address')
-            #employ_number = UserManager.normalize_email(employ_number)
-        user = self.model(employ_number=employ_number, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, employ_number, password):
-        return self.create_user(employ_number, password)
-
-
-class User(AbstractBaseUser):
-    employ_number = models.CharField(max_length=32, primary_key=True, null=False)
-    kaCode = models.CharField(max_length=32, null=False,default="0000")
-
-
-    USERNAME_FIELD = 'employ_number'
-
-    objects = UserManager()
-
-    class Meta:
-        db_table = 'waterdropapp_user'
-        swappable = 'AUTH_USER_MODEL'
-
+社員テーブル
 '''
 class User(AbstractUser):
     class Meta:
@@ -68,20 +42,6 @@ class User(AbstractUser):
     kaCode = models.ForeignKey(KaTable, on_delete=models.CASCADE, null=True, verbose_name="課コード", help_text="所属課を選択してください")
 
 
-'''単にこれは課に属することを表すテーブルだからイマイチ価値がない'''
-'''やっぱり継承するかカスタムするかのどっちかだな・・・
-class SyainTable(models.Model):
-    class Meta:
-        verbose_name = "社員所属課情報"
-        verbose_name_plural = '社員所属課情報'
-
-    #複数課に所属するときはどうするべかな
-    kaCode = models.ForeignKey(KaTable, on_delete=models.CASCADE, null=False, verbose_name="所属課", help_text="社員が所属する課を選択してください")
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="社員ナンバー", help_text="社員ナンバーを選択してください")
-
-    def __str__(self):
-        return str(self.user)
-'''
 '''
 タイムカードテーブル
 '''
@@ -104,8 +64,12 @@ class TimeCardTable(models.Model):
 作番テーブル
 '''
 class WorkCodeTable(models.Model):
-    work_code = models.CharField(max_length=32, primary_key=True, null=False)
-    name = models.CharField(max_length=128)
+    class Meta:
+        verbose_name = "作業番号"
+        verbose_name_plural = "作業番号"
+    dummy_id = models.AutoField(primary_key=True)
+    work_code = models.CharField(max_length=32, null=True, unique=True, verbose_name="作番コード")
+    name = models.CharField(max_length=128, verbose_name="作業内容")
 
 '''
 作業内容テーブル
@@ -118,6 +82,10 @@ class WorkDetailCodeTable(models.Model):
 作業テーブル
 '''
 class WorkTable(models.Model):
+    class Meta:
+        verbose_name = "作業一覧"
+        verbose_name_plural = "作業一覧"
+
     id = models.BigAutoField(primary_key=True, null=False)
     time_card_id = models.ForeignKey(TimeCardTable, on_delete=models.CASCADE, null=False)
     work_code = models.ForeignKey(WorkCodeTable, on_delete=models.CASCADE, null=False)
@@ -128,37 +96,104 @@ class WorkTable(models.Model):
 単価テーブル
 '''
 class PriceTable(models.Model):
+    class Meta:
+        verbose_name = "単価"
+        verbose_name_plural = "単価"
+
     tanka_id = models.BigAutoField(primary_key=True, null=False)
-    name = models.CharField(max_length=32, null=False)
-    tanka = models.FloatField()
+    name = models.CharField(max_length=32, null=False, verbose_name="単価名", help_text="社員の等級を設定します")
+    tanka = models.IntegerField(verbose_name="単価", help_text="等級の時給単価を設定します")
+
+    def __str__(self):
+        return self.name
 
 '''
-区分テーブル
+休暇区分テーブル
 '''
-class KubunTable(models.Model):
+class HolidayKubunTable(models.Model):
+    class Meta:
+        verbose_name = "休暇区分"
+        verbose_name_plural = "休暇区分"
+
     kubunid = models.AutoField(primary_key=True, null=False)
-    name = models.CharField(max_length=32, null=False)
-    div = models.FloatField()
+    name = models.CharField(max_length=32, null=False, verbose_name="休暇区分名", help_text="土日，祝日，夏季休暇など")
+
+    def __str__(self):
+        return self.name
+
+'''
+オフィステーブル
+'''
+class OfficeTable(models.Model):
+    class Meta:
+        verbose_name = "オフィス"
+        verbose_name_plural = "オフィス"
+
+    id = models.BigAutoField(primary_key=True, null=False)
+    name = models.CharField(max_length=32, null=False,verbose_name="オフィス名", help_text="事務所ごとに休日カレンダーを指定できます")
+
+    def __str__(self):
+        return self.name
 
 '''
 カレンダーテーブル
+休みだけ登録する
 '''
 class CalendarTable(models.Model):
+    class Meta:
+        verbose_name = "休日カレンダー"
+        verbose_name_plural = '休日カレンダー'
+
     id = models.BigAutoField(primary_key=True, null=False)
-    name = models.CharField(max_length=32, null=False)
-    year = models.IntegerField()
-    month = models.IntegerField()
-    day = models.IntegerField()
-    kubunid = models.ForeignKey(KubunTable, on_delete=models.CASCADE, null=False)
+    name = models.ForeignKey(OfficeTable, on_delete=models.CASCADE, null=False, verbose_name="オフィス名", help_text="カレンダーを適用するオフィスを選択します")
+    date = models.DateField( verbose_name="日付", help_text="休日を指定してください", null=True, unique=True)
+    Remark = models.CharField(max_length=128, null=True, verbose_name="備考", help_text="祝日名や夏休みなどを記入してください")
+    kubunid = models.ForeignKey(HolidayKubunTable, on_delete=models.CASCADE, null=False, verbose_name="休暇区分", help_text="休暇区分を指定します")
+
+    def __str__(self):
+        return str(self.date)
 
 '''
-明細テーブル
+交通手段
 '''
-class MeisaiTable(models.Model):
+class HowToMove(models.Model):
+    class Meta:
+        verbose_name = "交通手段"
+        verbose_name_plural = '交通手段'
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, verbose_name="移動手段", help_text="移動手段を入力します")
+
+    def __str__(self):
+        return self.name
+
+'''承認ステータス'''
+class ApproveStatus(models.Model):
+    class Meta:
+        verbose_name = "承認ステータス"
+        verbose_name_plural = "承認ステータス"
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64, verbose_name="承認ステータス")
+
+    def __str__(self):
+        return self.name
+
+class TransportExpense(models.Model):
+    class Meta:
+        verbose_name = "交通費"
+        verbose_name_plural = "交通費"
+
     id = models.BigAutoField(primary_key=True)
-    year = models.IntegerField()
-    month = models.IntegerField()
-    day = models.IntegerField()
-    calendar_id = models.ForeignKey(CalendarTable, on_delete=models.CASCADE, null=False)
-    employ_number = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    tana_id = models.ForeignKey(PriceTable, on_delete=models.CASCADE, null=False)
+    employee_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False, verbose_name="社員番号")
+    date = models.DateField(verbose_name="移動日")
+    adddate = models.DateTimeField(auto_now_add=True, verbose_name="申請日", null=True)
+    expense = models.IntegerField(default=0, verbose_name="交通費")
+    start = models.CharField(max_length=128,verbose_name="出発地")
+    end = models.CharField(max_length=128,verbose_name="目的地")
+    how = models.ForeignKey(HowToMove, on_delete=models.CASCADE, null=False, verbose_name="交通手段", help_text="交通手段を指定します（電車、タクシーなど)")
+    status = models.ForeignKey(ApproveStatus, on_delete=models.CASCADE, null=False, verbose_name="承認ステータス", help_text="承認ステータスを指定します")
+    remark = models.CharField(max_length=256, null=False)
+
+    def __str__(self):
+        return str(self.date)
+
